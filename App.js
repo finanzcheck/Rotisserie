170dfb8e79fbb5437636e4b87a11498dd07c1ca0
Robert Wawrzyniak <robert.wawrzyniak@finanzcheck.de>
2020-02-24 16:32:24 +0100

    feat: create basic statistics view

--- App.js
+++ App.js
@@ -1,74 +1,89 @@
 import React from 'react';
-import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {AsyncStorage, Button, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const key = "Rotis";
 
 const _storeData = async () => {
-  try {
-    await AsyncStorage.setItem(key, 'I like to save it.');
-  } catch (error) {
-    // Error saving data
-  }
+    try {
+        await AsyncStorage.setItem(key, 'I like to save it.');
+    } catch (error) {
+        // Error saving data
+    }
 };
 
 const _retrieveData = async () => {
-  try {
-    const value = await AsyncStorage.getItem(key);
-    if (value !== null) {
-      // We have data!!
-      console.log(value);
+    try {
+        const value = await AsyncStorage.getItem(key);
+        if (value !== null) {
+            // We have data!!
+            console.log(value);
+        }
+    } catch (error) {
+        // Error retrieving data
     }
-  } catch (error) {
-    // Error retrieving data
-  }
 };
 
-function HomeScreen({ navigation }) {
-  return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button onPress={_storeData}  title="Save"/>
-        <Button onPress={_retrieveData} title="Get"/>
-        <Button
-            title="Go to Roti"
-            onPress={() => navigation.navigate('Roti')}
-        />
-      </View>
-  );
+function HomeScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Button onPress={_storeData} title="Save"/>
+            <Button onPress={_retrieveData} title="Get"/>
+            <Button
+                title="Go to Roti"
+                onPress={() => navigation.navigate('Roti')}
+            />
+            <Button
+                title="Go to Stats"
+                onPress={() => navigation.navigate('Statistics')}
+            />
+        </View>
+    );
 }
 
-function NewRotiScreen({ navigation }) {
-  return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Roti whoop whoop</Text>
-        <Button
-            title="Go to Home"
-            onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-  )
+function NewRotiScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Roti whoop whoop</Text>
+            <Button
+                title="Go to Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
+    )
+}
+
+const Container = ({children}) => <View style={styles.container}>{children}</View>
+
+const Statistics = ({navigation}) => {
+    return (
+        <Container>
+            <Text>Big Data</Text>
+        </Container>
+    )
 }
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Roti" component={NewRotiScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Roti" component={NewRotiScreen}/>
+                <Stack.Screen name="Statistics" component={Statistics}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
