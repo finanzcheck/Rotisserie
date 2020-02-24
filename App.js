652a98277479d212be72ba43a6dfb2d62881239b
Daniel Lehmann <daniellehmann92@yahoo.de>
2020-02-24 15:47:18 +0100

    feat: add navigation and storage

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const key = "Rotis";
+
+const _storeData = async () => {
+  try {
+    await AsyncStorage.setItem(key, 'I like to save it.');
+  } catch (error) {
+    // Error saving data
+  }
+};
+
+const _retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};
+
+function HomeScreen({ navigation }) {
+  return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={_storeData}  title="Save"/>
+        <Button onPress={_retrieveData} title="Get"/>
+        <Button
+            title="Go to Roti"
+            onPress={() => navigation.navigate('Roti')}
+        />
+      </View>
+  );
+}
+
+function NewRotiScreen({ navigation }) {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Roti whoop whoop</Text>
+        <Button
+            title="Go to Home"
+            onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+  )
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Roti" component={NewRotiScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
